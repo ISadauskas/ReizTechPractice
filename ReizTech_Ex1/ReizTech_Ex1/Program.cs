894e5c6874de3ebfd2a265cc6c4e6c292999f461
ISadauskas <ignas.sadauskas7@gmail.com>
2022-01-24 12:05:35 +0200

    Both exercises completed

---
+++ ReizTech_Ex1/ReizTech_Ex1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ReizTech_Ex1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please imput the hours: ");
+            int hours = int.Parse(Console.ReadLine());
+            while(hours < 0 || hours > 12)
+            {
+                Console.WriteLine("Please imput a valid number of hours: ");
+                hours = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Please imput the minutes: ");
+            int minutes = int.Parse(Console.ReadLine());
+            while (minutes < 0 || minutes > 59)
+            {
+                Console.WriteLine("Please imput a valid number of minutes: ");
+                minutes = int.Parse(Console.ReadLine());
+            }
+
+            decimal minuteDegree = (360 / 60) * minutes;
+            decimal minutePercentage = (minuteDegree / 360m);
+            decimal hourDegree = (((360 / 12) * hours) + (30 * minutePercentage));
+                
+            decimal degree = Math.Round(Math.Abs(minuteDegree - hourDegree));
+            Console.WriteLine(degree);
+        }
+    }
+}
---
+++ ReizTech_Ex2/ReizTech_Ex2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+public class Node
+{
+	public int data;
+	public Node left, middle, right;
+
+	public Node(int item)
+	{
+		data = item;
+		left = right = middle = null;
+	}
+}
+
+public class Tree
+{
+	Node root;
+
+	int maxDepth(Node node)
+	{
+		if (node == null)
+			return -1;
+		else
+		{
+			int lDepth = maxDepth(node.left);
+			int rDepth = maxDepth(node.right);
+			int mDepth = maxDepth(node.middle);
+
+			if (lDepth > mDepth && lDepth > rDepth)
+				return lDepth + 1;
+			else if (rDepth > mDepth && rDepth > lDepth)
+				return rDepth + 1;
+			else
+				return mDepth + 1;		
+		}
+	}
+
+	public static void Main(String[] args)
+	{
+		Tree tree = new Tree();
+
+		tree.root = new Node(1);
+		tree.root.left = new Node(2);
+		tree.root.right = new Node(3);
+		tree.root.left.left = new Node(4);
+		tree.root.left.right = new Node(5);
+		tree.root.left.middle = new Node(6);
+		tree.root.left.middle.middle = new Node(7);
+		tree.root.left.middle.middle.middle = new Node(8);
+		tree.root.left.middle.middle.middle.middle = new Node(9);
+
+		int depth = 1 + tree.maxDepth(tree.root);
+
+		Console.WriteLine("Height of tree is : " + depth);
+	}
+}
+
